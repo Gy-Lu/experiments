52d7e426de8451895a7251bca950aeaccbf6adc4
luguangyang <luguangyang@kuaishou.com>
2024-08-15 17:36:02 +0800

    [mlsys] add megatron moe group

--- mlsys/megatron_group.py
+++ mlsys/megatron_group.py
@@ -2,11 +2,14 @@ DP_rank = []
 PP_rank = []
 TP_rank = []
 CP_rank = []
+EP_rank = []
+EP_and_TP_rank = []
 
 tensor_model_parallel_size = 4
 data_parallel_size = 4
 context_parallel_size = 2
 pipeline_model_parallel_size = 4
+expert_model_parallel_size = 4
 
 world_size = tensor_model_parallel_size * data_parallel_size * pipeline_model_parallel_size
 
@@ -37,7 +40,26 @@ for i in range(num_pipeline_model_parallel_groups):
     ranks = range(i, world_size, num_pipeline_model_parallel_groups)
     PP_rank.append(list(ranks))
 
+tensor_and_data_group_size: int = tensor_model_parallel_size * data_parallel_size
+num_tensor_and_data_groups: int = world_size // tensor_and_data_group_size
+tensor_and_expert_group_size: int = tensor_model_parallel_size * expert_model_parallel_size
+num_expert_groups: int = data_parallel_size // expert_model_parallel_size
+for i in range(num_tensor_and_data_groups):
+    for j in range(num_expert_groups):
+        # TPxEP Group
+        start_rank = i * tensor_and_data_group_size + j * tensor_and_expert_group_size
+        end_rank = i * tensor_and_data_group_size + (j + 1) * tensor_and_expert_group_size
+        ranks = range(start_rank, end_rank)
+        EP_and_TP_rank.append(list(ranks))
+        for k in range(tensor_model_parallel_size):
+            ranks = range(
+                start_rank + k, end_rank, tensor_model_parallel_size
+            )
+            EP_rank.append(list(ranks))
+
 print(f"TP rank: {TP_rank}")
 print(f"DP rank: {DP_rank}")
 print(f"CP rank: {CP_rank}")
 print(f"PP rank: {PP_rank}")
+print(f"EP and TP rank: {EP_and_TP_rank}")
+print(f"EP rank: {EP_rank}")
